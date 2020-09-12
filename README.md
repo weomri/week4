6f8e81de184303d8eb6a3337d73b635ea151eebd
weomri <omri87@gmail.com>
2020-09-12 16:10:25 +0300

    helpgit commit

--- README.md
+++
@@ -1,6 +0,0 @@
-# Peer-graded Assignment: Bash, Make, Git, and GitHub
-makefile was run on Sat Sep 12 15:57:21 2020 
-The number of code-lines in guessinggame.sh. is 
-# Peer-graded Assignment: Bash, Make, Git, and GitHub
-makefile was run on Sat Sep 12 15:57:48 2020 
-The number of code-lines in guessinggame.sh. is 31 guessinggame.sh
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
-
 countD=$(ls -1 -F -A | grep / | wc -l)
-
-echo "no. of dir: $countD"
-
 countA=$(ls -1 -F -A | wc -l)
-
-echo "no. of files including dir: $countA"
-
 countF=$(echo "$countA - $countD" | bc -l)
-
-echo "no. of files: $countF"
-
 echo "please guess how many files are in this folder"
-
 read num
-
-echo "$num"
-
+function guessAgain {
+	echo "please guess again"
+        read  num
+}
 while [[ $num -ne $countF ]]
 do
 	if [[ $num -gt $countF ]]
@@ -26,6 +16,6 @@ do
 	else
                 echo "you guessed too low of a number"
 	fi
-	echo "please guess again"
-	read  num
+	guessAgain
 done
+echo "congratulations! you guessed correctly"
